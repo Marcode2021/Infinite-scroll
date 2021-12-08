1acc7ff1d921bd02ad8d9846b366923e5d2b6ee8
Marcode2021 <marcocode2021@gmail.com>
2021-12-08 10:10:45 +0100

    Fetch data from API

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+// Unsplash API
+const count = 10;
+const apiKey = "VOVZ36Ty6M0jfOWM-5UFCle8Iw5sT-mLBDVzLlLiXfE";
+const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Get photos from API
+const getPhotos = async function () {
+  try {
+    const res = await fetch(apiURL);
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {
+    // Catch error here
+    console.error(err);
+  }
+};
+
+// Init function
+getPhotos();
